25401eb60b1b897cac02a3f4c05f3c1e466a049c
Mike Zens <mzens@activeingredients.com>
2018-07-27 12:31:03 -0600

    Adding require js support with wrapper

--- js/jquery.stickytableheaders.js
+++ js/jquery.stickytableheaders.js
@@ -1,7 +1,12 @@
-/*! Copyright (c) Jonas Mosbech - https://github.com/jmosbech/StickyTableHeaders
-	MIT license info: https://github.com/jmosbech/StickyTableHeaders/blob/master/license.txt */
-
-;(function ($, window, undefined) {
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module depending on jQuery.
+		define(['jquery'], factory);
+	} else {
+		// No AMD. Register plugin with global jQuery object.
+		factory(jQuery);
+	}
+}(function ($) {
 	'use strict';
 
 	var name = 'stickyTableHeaders',
@@ -71,7 +76,7 @@
 					'</style>');
 				base.$head.append(base.$printStyle);
 			});
-			
+
 			base.$clonedHeader.find("input, select").attr("disabled", true);
 
 			base.updateWidth();
@@ -321,5 +326,4 @@
 			}
 		});
 	};
-
-})(jQuery, window);
+}));
